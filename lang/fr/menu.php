3844630d862e96dea7f86871c88e423a37954a29
Aichanjimate2022 <NjimateAicha>
2025-04-03 00:25:50 +0000

    first commit

---
+++ lang/fr/menu.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trading Definition section Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+    
+    'pricing' => 'prix',
+    'calendar' => 'calendrier',
+
+    'about' => 'à propos',
+    'our_services' => "nos Services",
+    'our_team' => "notre équipe",
+    'faq' => "FAQ",
+
+    'contact' => "Contactez-nous",
+
+    'steps' => "étape par étape",
+    'testimonials' => "témoignages",
+
+    'terms' => 'Termes de service',
+    'privacy' => 'Politique de confidentialité',
+];
